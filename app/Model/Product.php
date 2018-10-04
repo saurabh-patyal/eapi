04df962517a7c460d0a12964fb713770ddb69b7e
saurabh-patyal <saurabh.patyal@gmail.com>
2018-10-04 19:21:20 +0530

    till relationship btween product & reviews done

--- app/Model/Product.php
+++ app/Model/Product.php
@@ -3,8 +3,10 @@
 namespace App\Model;
 
 use Illuminate\Database\Eloquent\Model;
-
+use App\Model\Review;
 class Product extends Model
 {
-    //
+    public function reviews(){
+      return $this->hasMany('App\Model\Review');
+    }
 }
